0cad8c815d45d0c8534b39d3ce699ecb9e4c0e44
Teemu Kaukoranta <teemu.kaukoranta@solita.fi>
2016-03-17 19:28:22 +0200

    Show the TeamList

--- Main.elm
+++ Main.elm
@@ -2,12 +2,48 @@ module Main (..) where
 
 import Html exposing (..)
 import StartApp.Simple as StartApp
-import MatchList
+-- import MatchList
+import TeamList
+import Signal exposing (..)
 
-main: Signal Html
+
+main : Signal Html
 main =
   StartApp.start
-    { model = MatchList.initialModel
-    , view = MatchList.view
-    , update = MatchList.update
+    { model = TeamList.initialModel
+    , view = TeamList.view
+    , update = TeamList.update
     }
+
+
+{-
+type alias Model =
+  { teamList : TeamList.Model
+  , matchList : MatchList.Model
+  }
+
+initialModel =
+  {
+  matchList = MatchList.initialModel
+  ,teamList = TeamList.initialModel
+  }
+
+
+type Action
+  = MatchUpdate MatchList.Action
+  | TeamUpdate TeamList.Action
+
+update action model =
+  case action of
+    MatchUpdate a ->
+      {model | matchList = MatchList.update a model.matchList.model}
+    TeamUpdate a ->
+      {model | teamList = TeamList.update a model.teamList.model}
+
+view : Address Action -> Model -> Html
+view address model =
+  div
+    []
+    [ TeamList.view (Signal.forwardTo address TeamUpdate) model.teamList
+    , MatchList.view (Signal.forwardTo address MatchUpdate) model.matchList
+    ]-}
